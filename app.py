8fd73237004df629df042629a15b187a54b7fab8
vitor-costa-garcia <vitorcostagarcia2006@gmail.com>
2026-03-25 10:19:13 -0300

    Fixed #3 -> add:db.py cria banco de dados da aplicação usando models dentro da pasta ./models + tarefas_models.py: model da tabela de tarefas da aplicação

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify
+from flask import render_template
+from db import create_app
 
 # Criação da instância para o aplicativo Flask.
-app = Flask(__name__)
+app = create_app()
 
 # Definição do Endpoint inicial, que retorna uma mensagem de sucesso e o código HTTP 200, que significa "OK"
 @app.route('/')
 def home():
-    return jsonify({"message": "Sistema de Gerenciamento de Tarefas Inicializado"}), 200
+    return render_template("index.html")
 
 # Inicialização do servidor Flask, com o modo de depuração ativado para facilitar o desenvolvimento e a identificação de erros.
 if __name__ == '__main__':
